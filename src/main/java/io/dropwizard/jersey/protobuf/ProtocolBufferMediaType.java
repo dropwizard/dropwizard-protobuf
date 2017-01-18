09a2623284249fe1528fe6b8c5c34cc682e1a3eb
Rob Paveza <rpaveza@blizzard.com>
2017-01-18 14:40:27 -0800

    Added support for protobuf text format serialization

--- src/main/java/io/dropwizard/jersey/protobuf/ProtocolBufferMediaType.java
+++ src/main/java/io/dropwizard/jersey/protobuf/ProtocolBufferMediaType.java
@@ -6,6 +6,9 @@ public class ProtocolBufferMediaType extends MediaType {
 
     /** "application/x-protobuf" */
     public final static String APPLICATION_PROTOBUF = "application/x-protobuf";
+    /** "application/x-protobuf,application/x-protobuf-text-format" */
+    public final static String APPLICATION_PROTOBUF_AND_TEXT_FORMAT = "application/x-protobuf,application/x-protobuf-text-format";
+
     /** "application/x-protobuf" */
     public final static MediaType APPLICATION_PROTOBUF_TYPE = new MediaType("application","x-protobuf");
 }
