73c43ec9d28f14ae82a6dea19217644c3ffa08d2
Jochen Schalanda <jochen@schalanda.name>
2023-12-21 22:40:15 +0100

    Code formatting

--- src/main/java/io/dropwizard/jersey/protobuf/ProtocolBufferMediaType.java
+++ src/main/java/io/dropwizard/jersey/protobuf/ProtocolBufferMediaType.java
@@ -21,18 +21,21 @@ public class ProtocolBufferMediaType extends MediaType {
 
   /** "application/x-protobuf" */
   public static final String APPLICATION_PROTOBUF = "application/x-protobuf";
+
   /** "application/x-protobuf" */
   public static final MediaType APPLICATION_PROTOBUF_TYPE =
       new MediaType("application", "x-protobuf");
 
   /** "application/x-protobuf-text-format" */
   public static final String APPLICATION_PROTOBUF_TEXT = "application/x-protobuf-text-format";
+
   /** "application/x-protobuf-text-format" */
   public static final MediaType APPLICATION_PROTOBUF_TEXT_TYPE =
       new MediaType("application", "x-protobuf-text-format");
 
   /** "application/x-protobuf-json-format" */
   public static final String APPLICATION_PROTOBUF_JSON = "application/x-protobuf-json-format";
+
   /** "application/x-protobuf-json-format" */
   public static final MediaType APPLICATION_PROTOBUF_JSON_TYPE =
       new MediaType("application", "x-protobuf-json-format");
